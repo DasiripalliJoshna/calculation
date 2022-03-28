df3220d24660890c0ad373306458f3baaa5e3467
Dasiripalli Joshna <91077542+DasiripalliJoshna@users.noreply.github.com>
2022-03-28 11:52:31 +0530

    Update test_calculation.py

--- test_calculation.py
+++ test_calculation.py
@@ -1,18 +1,22 @@
 import calculation
+import pytest
 
-def test_square():
-    a=10
-    result=calculation.square(a)
-    assert result == a*a
 
-def test_perimeter():
-    l=20
-    w=10
-    result=calculation.perimeter(l,w)
+def test_area_square():
+    side=3
+    result=calculation.area_square(side)
+    assert result == side * side
+
+@pytest.mark.perimeterarearec
+def test_perimeter_rect():
+    l=5
+    w=8
+    result=calculation.perimeter_rect(l,w)
     assert result == 2*(l+w)
 
-def test_area():
-    l=20
-    w=10
-    result=calculation.area(l*w)
-    assert result == l*w
+@pytest.mark.perimeterarearec
+def test_area_rect():
+    l=5
+    b=6
+    result=calculation.area_rect(l,b)
+    assert result == l*b
